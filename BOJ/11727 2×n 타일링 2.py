0310a8c4e531d7e617f784c85f13131d29b83413
Aerim <wjsdofla7882@ajou.ac.kr>
2022-07-27 21:57:51 +0800

    Create 11727.py 11726.py

---
+++ BOJ/11727 2×n 타일링 2.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+memory = []
+
+for i in range(n+1):
+  if i ==0 :
+    memory.append(0)
+  elif i == 1:
+    memory.append(1)
+  elif i == 2:
+    memory.append(3)
+  else:
+    memory.append(memory[i-1] + 2*memory[i-2])
+
+print(memory[n] % 10007)
